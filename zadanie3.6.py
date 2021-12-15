bc36fd6d9a797d4f888b7eaea1125a77ace14426
ThickGuy03 <61799837+ThickGuy03@users.noreply.github.com>
2021-12-15 19:52:34 +0100

    Lista 6 END

--- zadanie3.6.py
+++ zadanie3.6.py
@@ -1,5 +1,16 @@
-def generuj(n):
-    for i in range(1,n+1):
-         print("a["+str(i-1)+"]="+"look-and-say"*i)
-n=int(input("Podaj liczbe wyrazów ciągów które chcesz zobaczyć: "))
-generuj(n)
+def look_and_say(s):
+    wynik=[]
+    i=0
+    while i<len(s):
+        liczba=1
+        while i+1<len(s) and s[i]==s[i+1]:
+            i+=1
+            liczba+=1
+        wynik.append(str(liczba)+s[i])
+        i+=1
+    return ''.join(wynik)
+s="1"
+n=10
+for i in range(n):
+    s=look_and_say(s)
+    print(s)
